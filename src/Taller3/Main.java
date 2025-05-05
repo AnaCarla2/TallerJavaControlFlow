62f88a696c909118d4157131eb5f36c15bf6352b
AnaCarla2 <anacarlamartinezcaraballo@gmail.com>
2025-05-05 18:59:56 -0500

    Actualizo archivos en src con los cambios del taller

--- src/Taller3/Main.java
+++ src/Taller3/Main.java
@@ -1,12 +1,10 @@
-import Taller3.Task2;
-import Taller3.Task3;
-import Taller3.Task4;
+package Taller3;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-  //   Task1.superMercado();
+  //   Taller3.Task1.superMercado();
   //   Task2.veterinaria();
  //    Task3.parqueadero();
         Task4.tiendaRopa();
--- src/Taller3/Task1.java
+++ src/Taller3/Task1.java
@@ -1,3 +1,5 @@
+package Taller3;
+
 import java.util.Scanner;
 
 public class Task1 {
